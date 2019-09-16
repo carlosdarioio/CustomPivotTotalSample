16b19efacb8e2b764984cfd492302e5da11522fb
Carlos Dario Flores <cdfn3@hotmail.com>
2019-09-15 19:10:40 -0600

    Initial commit

---
+++ CustomPivotTotalSample/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace CustomPivotTotalSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+            this.pivotGridControl1 = new DevExpress.XtraPivotGrid.PivotGridControl();
+            this.pivotGridField1 = new DevExpress.XtraPivotGrid.PivotGridField();
+            this.pivotGridField2 = new DevExpress.XtraPivotGrid.PivotGridField();
+            this.pivotGridField3 = new DevExpress.XtraPivotGrid.PivotGridField();
+            this.pivotGridField4 = new DevExpress.XtraPivotGrid.PivotGridField();
+            this.layoutControlGroup1 = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
+            this.layoutControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pivotGridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // layoutControl1
+            // 
+            this.layoutControl1.Controls.Add(this.pivotGridControl1);
+            this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutControl1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControl1.Name = "layoutControl1";
+            this.layoutControl1.Root = this.layoutControlGroup1;
+            this.layoutControl1.Size = new System.Drawing.Size(589, 482);
+            this.layoutControl1.TabIndex = 0;
+            this.layoutControl1.Text = "layoutControl1";
+            // 
+            // pivotGridControl1
+            // 
+            this.pivotGridControl1.Fields.AddRange(new DevExpress.XtraPivotGrid.PivotGridField[] {
+            this.pivotGridField1,
+            this.pivotGridField2,
+            this.pivotGridField3,
+            this.pivotGridField4});
+            this.pivotGridControl1.Location = new System.Drawing.Point(12, 12);
+            this.pivotGridControl1.Name = "pivotGridControl1";
+            this.pivotGridControl1.Size = new System.Drawing.Size(565, 458);
+            this.pivotGridControl1.TabIndex = 4;
+            this.pivotGridControl1.CustomCellValue += new System.EventHandler<DevExpress.XtraPivotGrid.PivotCellValueEventArgs>(this.pivotGridControl1_CustomCellValue);
+            // 
+            // pivotGridField1
+            // 
+            this.pivotGridField1.Area = DevExpress.XtraPivotGrid.PivotArea.DataArea;
+            this.pivotGridField1.AreaIndex = 0;
+            this.pivotGridField1.FieldName = "Costo";
+            this.pivotGridField1.Name = "pivotGridField1";
+            // 
+            // pivotGridField2
+            // 
+            this.pivotGridField2.Area = DevExpress.XtraPivotGrid.PivotArea.RowArea;
+            this.pivotGridField2.AreaIndex = 1;
+            this.pivotGridField2.FieldName = "Producto";
+            this.pivotGridField2.Name = "pivotGridField2";
+            // 
+            // pivotGridField3
+            // 
+            this.pivotGridField3.Area = DevExpress.XtraPivotGrid.PivotArea.DataArea;
+            this.pivotGridField3.AreaIndex = 1;
+            this.pivotGridField3.FieldName = "Venta";
+            this.pivotGridField3.Name = "pivotGridField3";
+            // 
+            // pivotGridField4
+            // 
+            this.pivotGridField4.Area = DevExpress.XtraPivotGrid.PivotArea.RowArea;
+            this.pivotGridField4.AreaIndex = 0;
+            this.pivotGridField4.FieldName = "Factura";
+            this.pivotGridField4.Name = "pivotGridField4";
+            // 
+            // layoutControlGroup1
+            // 
+            this.layoutControlGroup1.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.layoutControlGroup1.GroupBordersVisible = false;
+            this.layoutControlGroup1.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem1});
+            this.layoutControlGroup1.Name = "layoutControlGroup1";
+            this.layoutControlGroup1.Size = new System.Drawing.Size(589, 482);
+            this.layoutControlGroup1.TextVisible = false;
+            // 
+            // layoutControlItem1
+            // 
+            this.layoutControlItem1.Control = this.pivotGridControl1;
+            this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem1.Name = "layoutControlItem1";
+            this.layoutControlItem1.Size = new System.Drawing.Size(569, 462);
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem1.TextVisible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 482);
+            this.Controls.Add(this.layoutControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
+            this.layoutControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pivotGridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraLayout.LayoutControl layoutControl1;
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup1;
+        private DevExpress.XtraPivotGrid.PivotGridControl pivotGridControl1;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
+        private DevExpress.XtraPivotGrid.PivotGridField pivotGridField1;
+        private DevExpress.XtraPivotGrid.PivotGridField pivotGridField2;
+        private DevExpress.XtraPivotGrid.PivotGridField pivotGridField3;
+        private DevExpress.XtraPivotGrid.PivotGridField pivotGridField4;
+    }
+}
+
---
+++ CustomPivotTotalSample/Form1.cs
@@ -0,0 +1,159 @@
+﻿using DevExpress.XtraPivotGrid;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomPivotTotalSample
+{
+    public partial class Form1 : Form
+    {
+        private DataSet xDataSet;
+        public Form1()
+        {
+            InitializeComponent();
+            //Llamando funciones
+            CrearTablaManual();           
+            BindData();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+
+
+        // Creando Tabla
+        private void CrearTablaManual()
+        {
+            // Crenado Nueva DataTable de facturas
+            DataTable xDT = new DataTable("Facturas");
+            DataColumn dataColumns;
+            DataRow xDataRow;
+
+            // Creando y nombrando columnas.    
+            dataColumns = new DataColumn();
+            dataColumns.DataType = typeof(String);
+            dataColumns.ColumnName = "Factura";
+            dataColumns.Caption = "Factura";
+            dataColumns.AutoIncrement = false;
+            dataColumns.ReadOnly = false;
+            dataColumns.Unique = false;            
+            xDT.Columns.Add(dataColumns);
+
+            
+            dataColumns = new DataColumn();
+            dataColumns.DataType = typeof(String);
+            dataColumns.ColumnName = "Producto";
+            dataColumns.Caption = "Producto";
+            dataColumns.AutoIncrement = false;
+            dataColumns.ReadOnly = false;
+            dataColumns.Unique = false;            
+            xDT.Columns.Add(dataColumns);
+
+            
+            dataColumns = new DataColumn();
+            dataColumns.DataType = typeof(double);
+            dataColumns.ColumnName = "Costo";
+            dataColumns.Caption = "Costo";
+            dataColumns.ReadOnly = false;
+            dataColumns.Unique = true;            
+            xDT.Columns.Add(dataColumns);
+
+            
+            dataColumns = new DataColumn();
+            dataColumns.DataType = typeof(double);
+            dataColumns.ColumnName = "Venta";
+            dataColumns.Caption = "Venta";
+            dataColumns.ReadOnly = false;
+            dataColumns.Unique = false;            
+            xDT.Columns.Add(dataColumns);
+            
+            // Creando DataSet  
+            xDataSet = new DataSet();
+            xDataSet.Tables.Add(xDT);
+
+            //Creando valores
+            xDataRow = xDT.NewRow();
+            xDataRow["Factura"] = "150";
+            xDataRow["Producto"] = "A20";
+            xDataRow["Costo"] = 4000;
+            xDataRow["Venta"] = 6000;
+            xDT.Rows.Add(xDataRow);
+
+            xDataRow = xDT.NewRow();
+            xDataRow["Factura"] = "150";
+            xDataRow["Producto"] = "A10";
+            xDataRow["Costo"] = 3000;
+            xDataRow["Venta"] = 4800;
+            xDT.Rows.Add(xDataRow);
+
+            xDataRow = xDT.NewRow();
+            xDataRow["Factura"] = "161";
+            xDataRow["Producto"] = "Portatil";
+            xDataRow["Costo"] = 3300;
+            xDataRow["Venta"] = 4500;
+            xDT.Rows.Add(xDataRow);
+
+            xDataRow = xDT.NewRow();
+            xDataRow["Factura"] = "161";
+            xDataRow["Producto"] = "Tablet";
+            xDataRow["Costo"] = 800;
+            xDataRow["Venta"] = 2000;
+            xDT.Rows.Add(xDataRow);
+        }        
+        private void BindData()
+        {            
+            DataColumn custCol = xDataSet.Tables["Facturas"].Columns["id"];            
+            BindingSource bs = new BindingSource();
+            bs.DataSource = xDataSet.Tables["Facturas"];            
+            pivotGridControl1.DataSource = bs;
+        }
+        /*
+         custom grand total pivot grid devexpress /  Personalizar Grand Total de un PivotGrid DevExpress c#
+         
+         Para poder mostrar este ejemplo de Devexpress c# primero crearemos una datatable manual
+         Ponerle valores y asignarsela a un pivotGridControl Para poder Personalizar el Total por columna de la tabla
+
+            Por ejemplo en la columna de costos mostraremos el porcentaje total que representa el costo por cada factura (Venta/costo)
+            mientras que en la de ventas mostraremos la ganacia total brindada por cada factura (Venta-costo)
+
+             */
+
+            //Funcion para personalizar totales
+        private void pivotGridControl1_CustomCellValue(object sender, DevExpress.XtraPivotGrid.PivotCellValueEventArgs e)
+        {
+            //Venta
+            double summaryValue1 = Convert.ToDouble(e.GetFieldValue(pivotGridField3).ToString());
+            //Costo
+            double summaryValue2 = Convert.ToDouble(e.GetFieldValue(pivotGridField1).ToString());
+            //Costo
+            if (e.DataField.Name == "pivotGridField1" && e.Value != null)
+            {
+                
+                //asignando el tipo de valor a modificar (Total)
+                if (e.RowValueType == PivotGridValueType.Total) //GrandTotal
+                {
+                    //obtendiendo valores                    
+                    e.Value = summaryValue1 / summaryValue2;
+                }
+            }
+
+            //Venta
+            if (e.DataField.Name == "pivotGridField3" && e.Value != null)
+            {
+                //asignando el tipo de valor a modificar (Total)
+                if (e.RowValueType == PivotGridValueType.Total) //GrandTotal
+                {                    
+                    e.Value = summaryValue1 - summaryValue2;
+                }
+            }
+        }
+    }
+}
